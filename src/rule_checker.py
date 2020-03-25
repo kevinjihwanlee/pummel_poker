f7236a409e3409b3a9e74ae15b8db21af512206c
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2020-03-24 23:35:34 -0500

    Adjust RuleChecker function implementations, plan for high card returns

--- src/rule_checker.py
+++ src/rule_checker.py
@@ -1,4 +1,4 @@
-import copy
+import copy, operator
 from game_objects import Card
 from constants import ace, king
 
@@ -6,47 +6,82 @@ class RuleChecker(object):
 	# analyze hand catch all method
 	# these should also return a high card
 
+	# TODO documentation
+	# this needs to return the hand as well as the relevant high card
 	def analyze_hand(self, hand, board):
 		both = hand + board
-		# straight flush
-		# quads
-		# full house
-		# flush
-		# straight
-		# trips
-		# two pair
-		# pair
-		# high card
+		both.sort()
+
+		is_sf, high = self.is_straight_flush(both)
+		if is_sf:
+			pass
+
+		is_q, high = self.is_quads(both)
+		if is_sf:
+			pass
+
+		is_fh, high = self.is_full_house(both)
+		if is_fh:
+			pass
+
+		is_fl, high = self.is_flush(both)
+		if is_fl:
+			pass
+		
+		is_str, high = self.is_straight(both)
+		if is_str:
+			pass
+
+		is_trip, high = self.is_trips(both)
+		if is_trip:
+			pass
+
+		is_twop, high = self.is_two_pair(both)
+		if is_two_p:
+			# TODO ties
+			pass
+
+		is_p, high = self.is_pair(both)
+		if is_p:
+			pass
+
+
 
 	def is_flush(self, hand):
 		suit_count = {}
 		for card in hand:
 			curr_suit = card.suit
 			suit_count[curr_suit] = suit_count[curr_suit] + 1 if curr_suit in suit_count else 1
-		return any(count >= 5 for count in suit_count.values())
+
+		# TODO just calculate the high card here
+
+		return (any(count >= 5 for count in suit_count.values()), max(suit_count.items(), key=operator.itemgetter(1))[0])
 
 	def is_straight(self, hand):
-		ordered_hand = copy.deepcopy(hand)
-		ordered_hand.sort()
-		end = len(ordered_hand)
+		end = len(hand)
 		min_len = 5
 		# edge case for A 2 3 4 5 straight
-		if ordered_hand[end - 1] == ace:
+		if hand[end - 1] == ace:
 			# if the first card is not a 2 and the second to last card is not a king
-			if ordered_hand[0] != 2 and ordered_hand[end - 2] != king:
+			if hand[0] != 2 and hand[end - 2] != king:
 				return False
 			end = end - 1
 			min_len = 4
 		longest = 1
 		counter = 1 
 		for i in range(1, end):
-			if ordered_hand[i].rank == ordered_hand[i - 1].rank + 1:
+			if hand[i].rank == hand[i - 1].rank + 1:
 				counter = counter + 1
 			else:
+				# straight ends here and the high card has to be recorded
 				counter = 1
 			longest = max(longest, counter)
+		
+		# TODO calculate where exactly the straight ends (look above)
+
 		return longest >= min_len
 	
 	def is_straight_flush(self, hand):
+		# TODO this needs to be adjusted according to the tuple returns
 		return self.is_flush(hand) and self.is_straight(hand)
 
--- src/rule_checker_test.py
+++ src/rule_checker_test.py
@@ -12,11 +12,13 @@ class TestRuleCheckerMethods(unittest.TestCase):
 		board = [Card(4, spades), Card(3, diamonds), Card(6, spades), Card(10, spades)]
 		board_with_flush = [Card(4, spades), Card(3, spades), Card(6, spades), Card(10, spades), Card(13, spades)]
 		hand_one.extend(board)
-		self.assertTrue(rc.is_flush(hand_one))
+		self.assertTrue(rc.is_flush(hand_one)[0])
+		self.assertEqual(rc.is_flush(hand_one)[1], spades)
 		hand_two.extend(board)
-		self.assertFalse(rc.is_flush(hand_two))
+		self.assertFalse(rc.is_flush(hand_two)[0])
 		hand_three.extend(board_with_flush)
-		self.assertTrue(rc.is_flush(hand_three))
+		self.assertTrue(rc.is_flush(hand_three)[0])
+		self.assertEqual(rc.is_flush(hand_one)[1], spades)
 
 	def test_straight(self):
 		rc = RuleChecker()
@@ -30,27 +32,35 @@ class TestRuleCheckerMethods(unittest.TestCase):
 		board_two = [Card(13, clubs), Card(12, clubs), Card(11, clubs), Card(10, clubs)]
 		board_three = [Card(3, clubs), Card(5, clubs), Card(10, diamonds), Card(11, spades), Card(4, hearts)]
 		hand_one.extend(board)
+		hand_one.sort()
 		self.assertTrue(rc.is_straight(hand_one))
 		hand_two.extend(board)
+		hand_two.sort()
 		self.assertFalse(rc.is_straight(hand_two))
 		hand_three.extend(board)
+		hand_three.sort()
 		self.assertTrue(rc.is_straight(hand_three))
 		hand_four.extend(board)
+		hand_four.sort()
 		self.assertFalse(rc.is_straight(hand_four))
 		hand_five.extend(board_two)
+		hand_five.sort()
 		self.assertTrue(rc.is_straight(hand_five))
 		hand_six.extend(board_three)
+		hand_six.sort()
 		self.assertTrue(rc.is_straight(hand_six))
 	
-	# def test_straight_flush(self):
-	# 	rc = RuleChecker()
-	# 	hand_one = [Card(2, spades), Card(5, spades)]
-	# 	hand_two = [Card(2, spades), Card(14, spades)]
-	# 	board = [Card(4, spades), Card(3, spades), Card(6, spades), Card(10, spades)]
-	# 	hand_one.extend(board)
-	# 	self.assertTrue(rc.is_straight_flush(hand_one))
-	# 	hand_two.extend(board)
-	# 	self.assertFalse(rc.is_straight_flush(hand_two))
+	def test_straight_flush(self):
+		rc = RuleChecker()
+		hand_one = [Card(2, spades), Card(5, spades)]
+		hand_two = [Card(2, spades), Card(14, spades)]
+		board = [Card(4, spades), Card(3, spades), Card(6, spades), Card(10, spades)]
+		hand_one.extend(board)
+		hand_one.sort()
+		self.assertTrue(rc.is_straight_flush(hand_one))
+		hand_two.extend(board)
+		hand_two.sort()
+		self.assertFalse(rc.is_straight_flush(hand_two))
 
 
 
